a520a2422ad96ab9f70ede45db7a518b8abdf59d
Mutuking <kawaguchi.2525.haruki@gmail.com>
2024-08-23 20:53:08 +0900

    04_02_確認課題

--- main.cpp
+++ main.cpp
@@ -913,11 +913,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		}
 		ImGui::End();
 
-
-
-		// 回転の処理
-		//transform.rotate.y += 0.005f;
-
 		Matrix4x4 worldMatrix = MakeAffineMatrix(transform.scale, transform.rotate, transform.translate);
 		Matrix4x4 cameraMatrix = MakeAffineMatrix(cameraTransform.scale, cameraTransform.rotate, cameraTransform.translate);
 		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
